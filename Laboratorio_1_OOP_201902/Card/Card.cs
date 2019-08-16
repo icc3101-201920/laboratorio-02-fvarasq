1d90cb32c1a182a0190b0529531bc688f9447ce7
fvarasq <fivaras@miuandes.cl>
2019-08-16 10:21:16 -0400

    Primer push

---
+++ Laboratorio_1_OOP_201902/Card/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902.Card
+{
+    public class Card
+    {
+        private string name;
+        private string type;
+        private string effect;
+        private int attackPoints;
+        private bool hero;
+
+    }
+}
---
+++ Laboratorio_1_OOP_201902/Hand.cs
@@ -0,0 +1,28 @@
+﻿using Laboratorio_1_OOP_201902.Card;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio_1_OOP_201902
+{
+    public class Hand: Deck
+    {
+        protected List<CombatCard> combatCards;
+        protected List<SpecialCard> SpecialCards;
+
+        public Hand(List<CombatCard> combatCards, List<SpecialCard> SpecialCards): private Deck()
+        {
+
+        }
+
+        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
+        public List<SpecialCard> SpecialCards1 { get => SpecialCards; set => SpecialCards = value; }
+
+        
+        public void AddCombatCard(CombatCard combatCard) { throw new NotImplementedException(); }
+        public void AddSpecialCard(SpecialCard specialCard) { throw new NotImplementedException(); }
+        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
+        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+        
+    }
+}
